e5a8446dd6dd5d8f493948bfaa0b94c546434712
Thanh Nguyen <puffle.nnt@gmail.com>
2022-12-02 16:42:30 +0700

    dev-18: Bottom Tab Navigation

--- src/libs/navigation/HomeTabBar.tsx
+++ src/libs/navigation/HomeTabBar.tsx
@@ -1,14 +1,68 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { HomeScreen, InsightScreen, SettingScreen } from 'screens/index'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+
+import colors from 'libs/ui/colors'
+import {
+  faChartColumn,
+  faGear,
+  faWallet
+} from '@fortawesome/free-solid-svg-icons'
 
 const Tab = createBottomTabNavigator<HomeTabBarParamList>()
 
 export const HomeTabBar = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
-      <Tab.Screen name="InsightScreen" component={InsightScreen} />
-      <Tab.Screen name="SettingScreen" component={SettingScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        headerShown: false
+      }}>
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: tabBarIconProps => (
+            <FontAwesomeIcon
+              color={
+                tabBarIconProps.focused ? colors.primary100 : colors.mono40
+              }
+              icon={faWallet}
+              size={24}
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="InsightScreen"
+        component={InsightScreen}
+        options={{
+          tabBarIcon: tabBarIconProps => (
+            <FontAwesomeIcon
+              color={
+                tabBarIconProps.focused ? colors.primary100 : colors.mono40
+              }
+              icon={faChartColumn}
+              size={24}
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="SettingScreen"
+        component={SettingScreen}
+        options={{
+          tabBarIcon: tabBarIconProps => (
+            <FontAwesomeIcon
+              color={
+                tabBarIconProps.focused ? colors.primary100 : colors.mono40
+              }
+              icon={faGear}
+              size={24}
+            />
+          )
+        }}
+      />
     </Tab.Navigator>
   )
 }
--- src/libs/navigation/MainStack.tsx
+++ src/libs/navigation/MainStack.tsx
@@ -1,11 +1,14 @@
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
-import {HomeTabBar} from './HomeTabBar'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { HomeTabBar } from './HomeTabBar'
 
 const Stack = createNativeStackNavigator<StackParamList>()
 
 export const MainStack = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false
+      }}>
       <Stack.Screen name="HomeTabBar" component={HomeTabBar} />
     </Stack.Navigator>
   )
