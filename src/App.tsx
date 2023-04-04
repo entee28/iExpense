5b18a41f14ef8c27ac3eab16e1744a6d50e8265a
Thanh Nguyen <puffle.nnt@gmail.com>
2023-04-04 13:56:12 +0700

    dev-70: disable graphql add __typename

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
 import { NavigationContainer } from '@react-navigation/native'
 import { MainStack } from 'libs/navigation'
 import { persistor, store } from 'libs/redux'
+import { toastConfig } from 'libs/ui'
 import React from 'react'
 import { StatusBar } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import Toast from 'react-native-toast-message'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import { toastConfig } from 'libs/ui'
 
 const client = new ApolloClient({
   uri: 'http://10.0.2.2:4000/graphql',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache({
+    addTypename: false
+  })
 })
 
 const App = () => {
